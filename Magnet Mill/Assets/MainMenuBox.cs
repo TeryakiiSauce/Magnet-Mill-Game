e3fb798c5e6d11c2836aadb3ecbbd9f647b25895
Albaraa-Janahi <92924482+Albaraa-Janahi@users.noreply.github.com>
2022-03-21 19:24:06 +0300

    working on main menu
    main menu

--- Magnet Mill/Assets/MainMenuBox.cs
+++ Magnet Mill/Assets/MainMenuBox.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class MainMenuBox : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public enum Direction { toRight, toLeft};
+    public Direction boxDirection;
+    public bool isUp;
+    private float rightMaxX = 24;
+    private float rightStartX = -17;
+    private float leftMinX = -20;
+    private float leftStartX = 24;
+    private float timer = 1.7f;
+    private Rigidbody body;
+    private ConstantForce constF;
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
+        constF = GetComponent<ConstantForce>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isUp)
+        {
+            constF.force = new Vector3(0, 50, 0);
+        }
+        else
+        {
+            constF.force = new Vector3(0, -50, 0);
+        }
+        Vector3 newPos = transform.localPosition;
+        if(boxDirection == Direction.toRight)
+        {
+            newPos.x+= speed * Time.deltaTime;
+            transform.localPosition = newPos;
+            if (transform.localPosition.x > rightMaxX)
+            {
+                transform.localPosition = new Vector3(rightStartX, 0.8f, transform.localPosition.x);
+            }
+        }
+        else
+        {
+            newPos.x -= speed * Time.deltaTime;
+            transform.localPosition = newPos;
+            if (transform.localPosition.x < leftMinX)
+            {
+                transform.localPosition = new Vector3(leftStartX, 0.8f, transform.localPosition.x);
+            }
+        }
+
+        if(timer < 1.7f)
+        {
+            timer += Time.deltaTime;
+            return;
+        }
+        isUp = !isUp;
+        timer = 0;
     }
 }
