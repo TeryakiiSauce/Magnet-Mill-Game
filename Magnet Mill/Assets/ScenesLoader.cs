b6b187afee1cf116e38c9a59ecbeb43479819839
Albaraa-Janahi <92924482+Albaraa-Janahi@users.noreply.github.com>
2022-03-24 17:40:57 +0300

    working on Scene loader

--- Magnet Mill/Assets/ScenesLoader.cs
+++ Magnet Mill/Assets/ScenesLoader.cs
@@ -8,8 +8,11 @@ public class ScenesLoader : MonoBehaviour
 {
     public static ScenesLoader instance;
     public Image blackImage;
-    public bool isTransitioning;
+    public float transitionSpeed;
+    public enum WhichScene { MainMenu, Level0, Level1, Level2, Level3};
     private string sceneName;
+    private enum action { none, show, hide };
+    private action currentAction;
     void Awake()
     {
         if(instance == null)
@@ -23,30 +26,62 @@ public class ScenesLoader : MonoBehaviour
         }
         DontDestroyOnLoad(gameObject);
     }
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if(isTransitioning && blackImage.color.a < 1)
+        if(currentAction == action.show)
         {
             Color tempClr = blackImage.color;
-            tempClr.a += Time.deltaTime;
+            tempClr.a += Time.deltaTime * transitionSpeed;
             blackImage.color = tempClr;
-            if(blackImage.color.a >= 1)
+            if(blackImage.color.a >= 1f)
             {
                 SceneManager.LoadScene(sceneName);
-                isTransitioning = false;            
+                currentAction = action.hide;
+            }
+        }
+        else if(currentAction == action.hide)
+        {
+            Color tempClr = blackImage.color;
+            tempClr.a -= Time.deltaTime * transitionSpeed;
+            blackImage.color = tempClr;
+            if(blackImage.color.a <= 0f)
+            {
+                currentAction = action.none;
+                blackImage.gameObject.SetActive(false);
             }
         }
     }
 
-    public void MoveToScene(string sceneName)
+    public void MoveToScene(WhichScene sceneName)
     {
-        isTransitioning = true;
-        this.sceneName = sceneName;
+        blackImage.gameObject.SetActive(true);
+        currentAction = action.show;
+        if(sceneName == WhichScene.MainMenu)
+        {
+            this.sceneName = "Main Menu";
+        }
+        else if(sceneName == WhichScene.Level0)
+        {
+            this.sceneName = "Level0";
+        }
+        else if(sceneName == WhichScene.Level1)
+        {
+            this.sceneName = "Level1";
+        }
+        else if (sceneName == WhichScene.Level2)
+        {
+            this.sceneName = "Level2";
+        }
+        else if (sceneName == WhichScene.Level3)
+        {
+            this.sceneName = "Level3";
+        }
+    }
+
+    public bool IsTransitioning()
+    {
+        return currentAction != action.none;
     }
 }
