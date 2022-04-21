96672b932435607fcb3a982b3001dbb135fda36e
TeryakiiSauce <48337702+TeryakiiSauce@users.noreply.github.com>
2022-04-21 03:32:56 +0300

    Added a random generator for items inside cube

--- Magnet Mill/Assets/Scripts/Characters/FixedRotation.cs
+++ Magnet Mill/Assets/Scripts/Characters/FixedRotation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FixRotation : MonoBehaviour
+public class FixedRotation : MonoBehaviour
 {
     Quaternion rotation;
     void Awake()
