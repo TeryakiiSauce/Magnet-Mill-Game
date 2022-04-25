d5615723ca9517db3b720d6f14a064996d145ef7
TeryakiiSauce <48337702+TeryakiiSauce@users.noreply.github.com>
2022-04-25 11:14:53 +0300

    Cleaned up CameraController.cs script

--- Magnet Mill/Assets/Scripts/Cameras/CameraController.cs
+++ Magnet Mill/Assets/Scripts/Cameras/CameraController.cs
@@ -5,13 +5,6 @@ using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
-    /*
-     * private GameObject[] quickCameras;
-    private CinemachineVirtualCamera quickTopCam, quickBottomCam, quickRightCam, quickLeftCam; // For the virtual cameras to be assigned from the array "quickCameras"
-    private CinemachineTransposer originalOffsetTopCam, originalOffsetBottomCam, originalOffsetRightCam, originalOffsetLeftCam; // To maintain original cameras' offsets
-    private CinemachineTransposer newOffsetTopCam, newOffsetBottomCam, newOffsetRightCam, newOffsetLeftCam; // To store new cameras' offsets
-    */
-
     public CinemachineVirtualCamera groundCam;
     public CinemachineVirtualCamera rightCam;
     public CinemachineVirtualCamera topCam;
@@ -22,10 +15,6 @@ public class CameraController : MonoBehaviour
     public CinemachineVirtualCamera lookLeftCam;
     public CinemachineVirtualCamera lookDownCam;
     public CinemachineVirtualCamera lookRightCam;
-
-    //public GameObject mainFocusPoint, groundFocusPoint, topFocusPoint, rightFocusPoint, leftFocusPoint; // For the quick virtual cameras aiming
-
-    //public GameObject mainFocus, groundFocus, topFocus, rightFocus, leftFocus; // For the quick virtual cameras aiming
 
     // Start is called before the first frame update
     void Start()
@@ -42,42 +31,13 @@ public class CameraController : MonoBehaviour
         lookLeftCam.Priority = 0;
         lookDownCam.Priority = 0;
         lookRightCam.Priority = 0;
-
-        /*quickCameras = GameObject.FindGameObjectsWithTag("Quick Cam View");
-        
-        // Assign the virtual cameras found from the array to their respective variables. The order is based on Unity's Hierarchy panel (left panel by default).
-        quickBottomCam = quickCameras[0].GetComponent<CinemachineVirtualCamera>();
-        quickRightCam = quickCameras[1].GetComponent<CinemachineVirtualCamera>();
-        quickTopCam = quickCameras[2].GetComponent<CinemachineVirtualCamera>();
-        quickLeftCam = quickCameras[3].GetComponent<CinemachineVirtualCamera>();
-
-        Testing
-        Debug.Log(quickBottomCam);
-        Debug.Log(quickRightCam);
-        Debug.Log(quickTopCam);
-        Debug.Log(quickLeftCam);
-
-        // Default LookAt value for the quick camera -> i.e. they should face the cube by default
-        quickBottomCam.LookAt = mainFocus.transform;
-        quickRightCam.LookAt = mainFocus.transform;
-        quickLeftCam.LookAt = mainFocus.transform;
-        quickTopCam.LookAt = mainFocus.transform;
-        
-        // Maintain original camera offset
-        originalOffsetTopCam = quickTopCam.GetComponent<CinemachineTransposer>();
-        originalOffsetBottomCam = quickBottomCam.GetComponent<CinemachineTransposer>();
-
-        newOffsetTopCam = groundFocus.GetComponent<CinemachineTransposer>();
-        newOffsetBottomCam = quickBottomCam.GetComponent<CinemachineTransposer>();*/
-
-        //quickTopCam.LookAt = mainFocusPoint.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         // For debugging only!
-        DebugCubePositionAndFlipStatus(true);
+        DebugCubePositionAndFlipStatus(false);
 
         // Idk why, but it felt more meaningful to check for the quick camera input before the main camera transition
         QuickCameraTransitionCheck();
