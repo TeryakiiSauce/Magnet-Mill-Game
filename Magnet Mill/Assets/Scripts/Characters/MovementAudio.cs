e6f4279ba60e25e9d299e307690eb2cb7617a3c8
TeryakiiSauce <48337702+TeryakiiSauce@users.noreply.github.com>
2022-04-26 18:18:22 +0300

    Cube movement causes a random sound effect

---
+++ Magnet Mill/Assets/Scripts/Characters/MovementAudio.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAudio : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public List<AudioClip> soundEffects;
+    public float volume = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        {
+            int randomSoundEffectIndex = Random.Range(0, soundEffects.Count);
+            audioSource.PlayOneShot(soundEffects[randomSoundEffectIndex], volume);
+        }
+    }
+}
