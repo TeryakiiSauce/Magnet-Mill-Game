48d076cb06c1b49780641947aad18d75080b9daf
TeryakiiSauce <48337702+TeryakiiSauce@users.noreply.github.com>
2022-04-28 16:59:42 +0300

    Added mouse parallax animation on all the menus

---
+++ Magnet Mill/Assets/Scripts/Other/Elias/MouseParallax.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseParallax : MonoBehaviour
+{
+    private Vector3 StartPos, contentPosition;
+
+    public float strength = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        contentPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        contentPosition.z = 0; // We don't need to move in the Z-axis
+
+        transform.position = new Vector3(StartPos.x + (contentPosition.x * strength), StartPos.y + (contentPosition.y * strength), 0);
+    }
+}
