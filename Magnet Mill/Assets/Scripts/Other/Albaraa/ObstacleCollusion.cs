25930a23edffe4631d438c293c1666fc0f3c7397
Albaraa-Janahi <92924482+Albaraa-Janahi@users.noreply.github.com>
2022-05-08 23:02:52 +0300

    Finished obstacles system

--- Magnet Mill/Assets/Scripts/Other/Albaraa/ObstacleCollusion.cs
+++ Magnet Mill/Assets/Scripts/Other/Albaraa/ObstacleCollusion.cs
@@ -4,7 +4,26 @@ using UnityEngine;
 
 public class ObstacleCollusion : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private Collider thisCol;
+
+    void Start()
+    {
+        thisCol = GetComponent<Collider>();
+    }
+
+    void Update()   //in update we will check if the obstacle is near the user, if yes we will turn on the collider,
+    {               //if no we will turn off the collider, this will save memory
+        if(!thisCol.enabled && Vector3.Distance(transform.position, GameController.instance.CubePosition()) <= 3)
+        {
+            thisCol.enabled = true;
+        }
+        else if(thisCol.enabled && Vector3.Distance(transform.position, GameController.instance.CubePosition()) > 3)
+        {
+            thisCol.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) //Check if the obstacle hit the cube, if yes send a message to the game controller
     {
         if(other.tag == "PlayerCube")
         {
