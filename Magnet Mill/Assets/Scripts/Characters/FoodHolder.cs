788848e5dc04a092f27d7dfedb6393ec46d3126b
Albaraa-Janahi <92924482+Albaraa-Janahi@users.noreply.github.com>
2022-05-10 18:07:29 +0300

    Removed food from box in the tutorial

--- Magnet Mill/Assets/Scripts/Characters/FoodHolder.cs
+++ Magnet Mill/Assets/Scripts/Characters/FoodHolder.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class FoodHolder : MonoBehaviour
 {
     public GameObject[] items;
+    public bool isInTutorial;
     void Awake()
     {
+        if (isInTutorial) return;
         int randIND = Random.Range(0, items.Length);
         items[randIND].SetActive(true);
     }
