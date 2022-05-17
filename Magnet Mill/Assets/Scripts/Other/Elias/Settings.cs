5fd6d62ef7525657f3e3845535b9183218919bd2
TeryakiiSauce <48337702+TeryakiiSauce@users.noreply.github.com>
2022-05-17 22:39:20 +0300

    Added settings option for mouse parallax

--- Magnet Mill/Assets/Scripts/Other/Elias/Settings.cs
+++ Magnet Mill/Assets/Scripts/Other/Elias/Settings.cs
@@ -9,12 +9,36 @@ using TMPro;
 public class Settings : MonoBehaviour
 {
     public GameObject settingsCanvas;
-    public TextMeshProUGUI qualitySelectedLabel, renderDistSelectedLabel;
+    public TextMeshProUGUI qualitySelectedLabel, renderDistSelectedLabel, mouseParallaxSelectedLabel;
 
-    private int defaultRenderDistance = 110, defaultQuality;
-    private GameObject qualitySliderGameObject; private Slider qualitySlider;
-    private GameObject maxRenderDistSliderGameObject; private Slider maxRenderDistSlider;
     private GameObject[] virtualCamerasGO;
+
+    // Default values
+    private int defaultQuality, defaultRenderDistance = 110, defaultMouseParallax = 4;
+
+    // Game objects & their slider component
+    private GameObject qualitySliderGO; private Slider qualitySlider;
+    private GameObject maxRenderDistSliderGO; private Slider maxRenderDistSlider;
+    private GameObject mouseParallaxSliderGO; private Slider mouseParallaxSlider;
+
+    // private void Awake()
+    // {
+    //     settingsCanvas = new GameObject();
+
+    //     Debugging(true);
+
+    //     if (SceneManager.GetActiveScene().name == "Main Menu")
+    //     {
+    //         if (!PlayerPrefs.HasKey("isFirstRun"))
+    //         {
+    //             InitializeUserSettings();
+    //         }
+    //     }
+    //     else
+    //     {
+    //         LoadLevelSettings();
+    //     }
+    // }
 
     // Start is called before the first frame update
     void Start()
@@ -38,25 +62,28 @@ public class Settings : MonoBehaviour
 
     private void InitializeUserSettings()
     {
-        //if () { PlayerPrefs.SetString("isFirstRun", "false"); return; }
-
         defaultQuality = QualitySettings.GetQualityLevel();
         PlayerPrefs.SetInt("qualitySetting", defaultQuality);
         PlayerPrefs.SetInt("maxRenderDistance", defaultRenderDistance);
+        PlayerPrefs.SetInt("mouseParallax", defaultMouseParallax * 5);
 
         PlayerPrefs.SetString("isFirstRun", "false");
     }
 
+    // When button is pressed from the main menu or pause menu
     public void SettingsButtonClicked()
     {
         settingsCanvas.gameObject.SetActive(true);
 
-        qualitySliderGameObject = GameObject.FindGameObjectWithTag("QualitySettingSlider");
-        qualitySlider = qualitySliderGameObject.GetComponent<Slider>();
+        qualitySliderGO = GameObject.FindGameObjectWithTag("QualitySettingSlider");
+        qualitySlider = qualitySliderGO.GetComponent<Slider>();
 
 
-        maxRenderDistSliderGameObject = GameObject.FindGameObjectWithTag("MaxRenderDistanceSetting");
-        maxRenderDistSlider = maxRenderDistSliderGameObject.GetComponent<Slider>();
+        maxRenderDistSliderGO = GameObject.FindGameObjectWithTag("MaxRenderDistanceSetting");
+        maxRenderDistSlider = maxRenderDistSliderGO.GetComponent<Slider>();
+
+        mouseParallaxSliderGO = GameObject.FindGameObjectWithTag("MouseParallaxSetting");
+        mouseParallaxSlider = mouseParallaxSliderGO.GetComponent<Slider>();
 
         LoadSettings();
     }
@@ -90,6 +117,7 @@ public class Settings : MonoBehaviour
     }
 
     // Loads settings for user to change when paused or in main menu
+    // Checks if user settings preferences keys exist and uses them; otherwise, it creates new keys
     private void LoadSettings()
     {
         if (PlayerPrefs.HasKey("qualitySetting"))
@@ -116,6 +144,19 @@ public class Settings : MonoBehaviour
         }
         RefreshLabel("render distance");
 
+
+        if (PlayerPrefs.HasKey("mouseParallax"))
+        {
+            mouseParallaxSlider.value = PlayerPrefs.GetInt("mouseParallax") / 5;
+        }
+        else
+        {
+            mouseParallaxSlider.value = defaultMouseParallax;
+            PlayerPrefs.SetInt("mouseParallax", ((int)mouseParallaxSlider.value) * 5);
+        }
+        RefreshLabel("mouse parallax");
+
+
     }
 
     private void SaveSettings()
@@ -124,6 +165,8 @@ public class Settings : MonoBehaviour
         PlayerPrefs.SetInt("qualitySetting", ((int)qualitySlider.value));
 
         PlayerPrefs.SetInt("maxRenderDistance", ((int)maxRenderDistSlider.value));
+
+        PlayerPrefs.SetInt("mouseParallax", ((int)mouseParallaxSlider.value) * 5);
     }
 
     private void ResetSettings()
@@ -131,6 +174,7 @@ public class Settings : MonoBehaviour
         PlayerPrefs.DeleteKey("isFirstRun");
         PlayerPrefs.DeleteKey("qualitySetting");
         PlayerPrefs.DeleteKey("maxRenderDistance");
+        PlayerPrefs.DeleteKey("mouseParallax");
 
         InitializeUserSettings();
     }
@@ -141,6 +185,55 @@ public class Settings : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    public void RefreshLabel(string forWhat)
+    {
+        switch (forWhat)
+        {
+            case "quality":
+                qualitySelectedLabel.text = QualitySettings.names[((int)qualitySlider.value)];
+                break;
+
+            case "render distance":
+                renderDistSelectedLabel.text = maxRenderDistSlider.value.ToString() + " meters";
+                break;
+
+            case "mouse parallax":
+                int tempMouseParallaxSetting = ((int)mouseParallaxSlider.value);
+                string tempStr = "";
+                mouseParallaxSelectedLabel.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+                switch (tempMouseParallaxSetting)
+                {
+                    case 0:
+                        tempStr = "Off";
+                        break;
+                    case 1:
+                        tempStr = "Low";
+                        break;
+                    case 2:
+                        tempStr = "Medium";
+                        break;
+                    case 3:
+                        tempStr = "High";
+                        break;
+                    case 4:
+                        tempStr = "Shake that mouse!";
+                        mouseParallaxSelectedLabel.transform.Rotate(0, 0, 4, Space.World);
+                        break;
+                    default:
+                        Debug.LogError("Something went wrong in the script [Settings.cs]... -> Go to RefreshLabel() -> Mouse Parallax");
+                        break;
+                }
+
+                mouseParallaxSelectedLabel.text = tempStr;
+                break;
+
+            default:
+                Debug.LogError("Something went wrong in the script [Settings.cs]... -> Go to RefreshLabel()");
+                break;
+        }
+    }
+
     private void Debugging(bool isActive)
     {
         if (isActive)
@@ -149,21 +242,33 @@ public class Settings : MonoBehaviour
             Debug.Log($"First time?: {PlayerPrefs.GetString("isFirstRun")}");
             Debug.Log($"Quality: {QualitySettings.names[PlayerPrefs.GetInt("qualitySetting")]}");
             Debug.Log($"Max Render Distance: {PlayerPrefs.GetInt("maxRenderDistance")} meters");
-        }
-    }
 
-    public void RefreshLabel(string forWhat)
-    {
-        switch (forWhat)
-        {
-            case "quality":
-                qualitySelectedLabel.text = QualitySettings.names[((int)qualitySlider.value)];
-                break;
-            case "render distance":
-                renderDistSelectedLabel.text = maxRenderDistSlider.value.ToString() + " meters";
-                break;
-            default:
-                break;
+            int tempMouseParallaxSetting = PlayerPrefs.GetInt("mouseParallax") / 5;
+            string tempStr = "";
+
+            switch (tempMouseParallaxSetting)
+            {
+                case 0:
+                    tempStr = "Off";
+                    break;
+                case 1:
+                    tempStr = "Low";
+                    break;
+                case 2:
+                    tempStr = "Medium";
+                    break;
+                case 3:
+                    tempStr = "High";
+                    break;
+                case 4:
+                    tempStr = "Shake that mouse!";
+                    break;
+                default:
+                    Debug.LogError("Something went wrong in the script [Settings.cs]... -> Go to Debugging()");
+                    break;
+            }
+
+            Debug.Log($"Mouse Parallax: {tempStr}");
         }
     }
 }
