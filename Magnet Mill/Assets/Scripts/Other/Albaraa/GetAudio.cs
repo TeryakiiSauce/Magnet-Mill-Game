4d157a17d0a9da4886a6a7b791dc5d66b1ded8b0
Albaraa-Janahi <92924482+Albaraa-Janahi@users.noreply.github.com>
2022-06-11 13:03:23 +0300

    Fixd car sound issue in freeze ability + added cheatCode

--- Magnet Mill/Assets/Scripts/Other/Albaraa/GetAudio.cs
+++ Magnet Mill/Assets/Scripts/Other/Albaraa/GetAudio.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class GetAudio : MonoBehaviour
 {
     public string soundName;
+    public bool stopWhenFreeze;
     void Start()
     {
         AudioManager.instance.GetAudio(gameObject, soundName);  //get audio source from audio manager
         AudioManager.instance.Play(soundName);
     }
+
+    void Update()
+    {
+        if (!stopWhenFreeze) return;
+
+        if(AudioManager.instance.IsPlaying(soundName) && AbilityController.instance.IsFreezeActive())
+        {
+            AudioManager.instance.Stop(soundName);
+        }
+        else if(!AudioManager.instance.IsPlaying(soundName) && !AbilityController.instance.IsFreezeActive())
+        {
+            AudioManager.instance.Play(soundName);
+        }
+    }
+
 }
